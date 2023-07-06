cb4cb075a8ef5c7f4f2251926b5b8bd1c4c88eb2
Aman Kumar <amankr.snp1@gmail.com>
2023-07-06 23:24:23 +0530

    initial commit

---
+++ src/main/java/com/aman/springdemo/SpringBootDockerDevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aman.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerDevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerDevopsDemoApplication.class, args);
+	}
+
+}
