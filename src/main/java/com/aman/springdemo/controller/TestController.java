3587e19514ff59f93d66ae7cf98f30bbfd717b2c
Aman Kumar <amankr.snp1@gmail.com>
2023-07-07 00:02:46 +0530

    Added test controller

---
+++ src/main/java/com/aman/springdemo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.aman.springdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello World!!";
+    }
+}
